85e44d9a85d3d05d074db8ba6a59adf0543ff5de
Michael Moradi <themoradis1@gmail.com>
2022-01-06 14:46:23 -0500

    First version of Ramaan navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from './pages/Home'
+import Posts from './pages/Posts'
 
 function App() {
   return (
-    <>  
-      <Header />
-      <div className='container'>
-        <h1>My App</h1>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/posts' element={<Posts />} />
+          </Routes>
+        </main>
       </div>
-    </>
+    </Router>
   )
 }
 
