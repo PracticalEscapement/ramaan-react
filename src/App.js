bdf1617b8279006adf9d888fd8d361e6d5d22b01
Michael Moradi <themoradis1@gmail.com>
2022-01-15 22:03:14 -0500

    The tailwind and daisyUI classes were removed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Posts from './pages/Posts'
 function App() {
   return (
     <Router>
-      <div className='flex flex-col justify-between h-screen'>
+      <div>
         <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
+        <main>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/posts' element={<Posts />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className='navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box'>
-      <div className='container mx-auto'>
-        <div>
-          RAMAAN
-        </div>
-        <div className='flex-none px-2 mx-2'>
-          <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-            Home
-          </Link>
-          <Link to='/posts' className='btn btn-ghost btn-sm rounded-btn'>
-            Posts
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <h1>Navbar</h1>
   )
 }
 
