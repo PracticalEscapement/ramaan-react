672b93b187124afc077151558dd3a3966f4f9730
Michael Moradi <themoradis1@gmail.com>
2022-01-17 17:17:14 -0500

    Navbar with flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,47 @@
-import { Link, useLocation } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 function Navbar() {
-  
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true
+    }
+  }
 
   return (
-    <div>
-      <ul className='navbar-elements'>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/posts'>Posts</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-      </ul>
-    </div>
+    <header className='navbar'>
+      <nav className='navbarNav'>
+        <ul className='navbarListItems'>
+          <li className='logo'>RAMAAN</li>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
+            <h3 className={
+              pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'
+            }
+            >
+              Home
+            </h3>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/posts')}>
+            <h3 className={
+              pathMatchRoute('/posts') ? 'navbarListItemNameActive' : 'navbarListItemName'
+            }
+            >
+              Posts
+            </h3>
+          </li>
+          <li className='navbarListItem' onClick={() => navigate('/about')}>
+            <h3 className={
+              pathMatchRoute('/about') ? 'navbarListItemNameActive' : 'navbarListItemName'
+            }
+            >
+              About
+            </h3>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
