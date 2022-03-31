ad29d5ddd00920e221c5c94e6369f156fb6dda07
Michael Moradi <69914892+PracticalEscapement@users.noreply.github.com>
2022-03-31 11:58:33 -0400

    Add comments (#15)
    * temp commit
    
    * Add comments refactor

---
+++ src/components/AddCommentForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { addComment } from '../context/RamaanActions'
+
+function AddCommentForm({ post_id, addNewComment }) {
+
+  const [commentData, setCommentData] = useState({
+    text: '',
+    likes: 0,
+  })
+
+  const onChange = (e) => {
+    setCommentData({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const callAddComment = async() => {
+      const response = await addComment(e, post_id, commentData)
+      addNewComment(response.data)
+    }
+    callAddComment()
+  }
+
+  return (
+    <div className='commentFormContainer'>
+      <form onSubmit={onSubmit}>
+        <textarea 
+          className='commentText'
+          placeholder='Let it all out...'
+          type='text'
+          id='text'
+          value={commentData.text}
+          onChange={onChange}
+        />
+        <button className='commentSubmitButton'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddCommentForm
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,26 +1,36 @@
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState, useCallback } from 'react'
 import RamaanContext from '../context/RamaanContext'
+import CurrentUserContext from '../context/user/currentUserContext'
 import { getPost } from '../context/RamaanActions.js'
+import Spinner from '../components/assests/Spinner'
+import AddCommentForm from '../components/AddCommentForm'
+import CommentsList from '../components/CommentsList'
 import { 
   VStack, 
   Box, 
   Image, 
   Heading,
-  Text,
-  Flex,
-  Avatar,
-  Divider
+  Text
 } from '@chakra-ui/react'
+
 
 function Post() {
   const { post, loading, dispatch} = useContext(RamaanContext)
-
-  const {title, review, image_url} = post
+  const { currentUser } = useContext(CurrentUserContext)
+  const { title, review, image_url } = post
 
   const [comments, setComments] = useState([])
+  const [buttonClicked, setButtonClicked] = useState(false)
+
+  const handleClick = () => {
+    setButtonClicked(true)
+  }
+  
+  const addNewComment = useCallback((newComment) => {
+    setComments([...comments, newComment])
+    setButtonClicked(false)
+  }, [comments])
 
   const params = useParams()
 
@@ -28,14 +38,18 @@ function Post() {
     dispatch({type: 'SET_LOADING'})
     const getPostObject = async() => {
       const postObject = await getPost(params.id)
-      setComments(postObject.comments)
       dispatch({type: 'GET_POST', payload: postObject})
+      setComments(postObject.comments)
     }
 
     getPostObject()
-  }, [dispatch])
+  }, [dispatch, params.id])
 
   const IMAGE = 'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
+  
+  if (loading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -49,32 +63,24 @@ function Post() {
           width={900}
           objectFit={'cover'} 
           src={IMAGE} 
-          alt='Restaurat Photo' 
+          alt='Restaurat Photo'
+          shadow={'2xl'}
         />
         <Box w={'900px'}>
           <Text mt={5} fontSize={'lg'}>
             {review}
           </Text>
         </Box>
-      
 
-      
-        <VStack align={'right'}>
-          <Divider mt={'50px'} />
-          <Heading fontSize={'lg'} align={'center'} mb={'20px'}>Comments</Heading>
-
-          <Box w={'800px'}>
-            {comments.map((comment) => (
-              <Flex key={comment.id} mb={'5'}>
-                <Avatar />
-                <Box>
-                  <Text ml={'5'}>{comment.user_id}</Text>
-                  <Text ml={'5'}>{comment.text}</Text>
-                </Box>
-              </Flex>
-            ))}
-          </Box>
-        </VStack>
+        <div className='addCommentsContainer'>
+          {buttonClicked
+            ? <AddCommentForm post_id={params.id} addNewComment={addNewComment} />
+            : <button className='addCommentButton' onClick={handleClick}>Add a Comment</button>
+          }
+        </div>
+        
+        <CommentsList comments={comments} />
+        
     
       </VStack>
     
